6aae88787395acb569ece0c2634fdd7706841597
ckorir <chrisruto.k@gmail.com>
2023-09-23 11:37:46 +0300

    Added grade generator index content

---
+++ Grade-Generator/index.js
@@ -0,0 +1,12 @@
+const readline = require("readline");
+const getGrade = require("./grades");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter the student's mark (between 0 and 100): ", (mark) => {
+  console.log(getGrade(mark));
+  rl.close();
+});
