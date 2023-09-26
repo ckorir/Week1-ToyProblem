b21bdcf577eb3e4dd5a621ceaaeedabc29a91975
ckorir <61984170+ckorir@users.noreply.github.com>
2023-09-26 09:57:34 +0300

    Added comments to salary index

--- Salary-Calculator/index.js
+++ Salary-Calculator/index.js
@@ -1,11 +1,12 @@
 const readline = require("readline");
+// imports functions in the salary file
 const getSalary = require("./salary");
-
+// allows user to enter data and processes it
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
+// Prompts user to enter some data based on the question
 rl.question("\n Enter your Basic Salary: ", (salary) => {
     console.log(getSalary(salary));
     rl.close();
