aad147760af7c735d1ceef78a3339672286f6f18
ckorir <61984170+ckorir@users.noreply.github.com>
2023-09-26 09:59:12 +0300

    Added grades to speed index

--- Grade-Generator/index.js
+++ Grade-Generator/index.js
@@ -1,11 +1,12 @@
 const readline = require("readline");
+// imports functions in the grades file
 const getGrade = require("./grades");
-
+// allows user to enter data and processes it
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
+// Prompts user to enter some data based on the question
 rl.question("Enter the student's mark (between 0 and 100): ", (mark) => {
   console.log(getGrade(mark));
   rl.close();
