0db151d646a0064ba4a31a5211eb76ad0f40317e
ckorir <61984170+ckorir@users.noreply.github.com>
2023-09-26 09:58:57 +0300

    Added comments to speed index

--- Speed-Detector/index.js
+++ Speed-Detector/index.js
@@ -1,11 +1,12 @@
 const readline = require("readline");
+// imports functions in the speed file
 const getSpeed = require("./speed");
-
+// allows user to enter data and processes it
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
+// Prompts user to enter some data based on the question
 rl.question("Enter the speed of the car in km/s: ", (speedinput) => {
   console.log(getSpeed(speedinput));
   rl.close();
